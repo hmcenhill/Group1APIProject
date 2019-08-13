79b86646e92951b1187942b7cd4a568e8560a5ca
Hermann I McEnhill <hmcenhill@gmail.com>
2019-08-13 14:55:32 -0400

    Recipe model, Startup maintenance for session and API framework to Home Controller

---
+++ Group1APIProject/Models/Recipe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Group1APIProject.Models
+{
+
+    public class RecipeHeader
+    {
+        public string Title { get; set; }
+        public float Version { get; set; }
+        public string Href { get; set; }
+        public Recipe[] Recipes { get; set; }
+    }
+
+    public class Recipe
+    {
+        public string Title { get; set; }
+        public string Href { get; set; }
+        public string Ingredients { get; set; }
+        public string Thumbnail { get; set; }
+    }
+}
