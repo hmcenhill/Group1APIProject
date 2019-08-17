06904ed290e550596c4678cd755700c0f78e7780
ernest2358 <ernest.h.bell@gmail.com>
2019-08-17 11:07:15 -0400

    Requirements met, needs style and flavor

--- Group1APIProject/Migrations/Data/20190817050612_New project.Designer.cs
+++ Group1APIProject/Migrations/Data/20190817050612_New project.Designer.cs
@@ -3,14 +3,16 @@ using Group1APIProject.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Group1APIProject.Migrations.Data
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20190817050612_New project")]
+    partial class Newproject
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Group1APIProject/Migrations/Data/20190817050612_New project.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Group1APIProject.Migrations.Data
+{
+    public partial class Newproject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeFavorites_ResultId",
+                table: "RecipeFavorites",
+                column: "ResultId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RecipeFavorites_Result_ResultId",
+                table: "RecipeFavorites",
+                column: "ResultId",
+                principalTable: "Result",
+                principalColumn: "ResultId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RecipeFavorites_Result_ResultId",
+                table: "RecipeFavorites");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RecipeFavorites_ResultId",
+                table: "RecipeFavorites");
+        }
+    }
+}
