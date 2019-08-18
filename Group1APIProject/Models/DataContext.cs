06ecc7d629411e28d545f79943a1ecad1d788171
chicoarellano <hi.rez.jon@gmail.com>
2019-08-18 19:37:23 -0400

    adding hyperlinks

--- Group1APIProject/Models/DataContext.cs
+++ Group1APIProject/Models/DataContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Group1APIProject.Models
@@ -14,6 +15,11 @@ namespace Group1APIProject.Models
         public virtual DbSet<UserData> Users { get; set; }
         public virtual DbSet<Result> Result { get; set; }
         public virtual DbSet<RecipeFavorite> RecipeFavorites { get; set; }
+
+        internal Task GetUserIdAsync(ClaimsPrincipal user)
+        {
+            throw new NotImplementedException();
+        }
 
         //public virtual DbSet<TicketmasterEvent> TicketmasterEvents { get; set; }
         //public virtual DbSet<TicketmasterEventFavorite> TicketmasterEventFavorites { get; set; }
